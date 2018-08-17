91e638066d122992241358aadacde94655c82ee4
eBLDR <ed.bldr@gmail.com>
2018-08-17 15:26:44 +0200

    ceil and floor methods added

--- DataScience_MathModules/mathmodule.py
+++ DataScience_MathModules/mathmodule.py
@@ -4,10 +4,10 @@ math module.
 
 import math
 
-# factorial
+# Factorial
 print(math.factorial(5))
 
-# CONSTANTS
+# --- CONSTANTS ---
 # euler number
 print("e =", math.e)
 
@@ -23,18 +23,25 @@ print(math.inf)     # equivalent to float('inf')
 # floating-point 'not a number' (NaN)
 print(math.nan)     # equivalent to float('nan')
 
-# POWER AND LOGARITHMIC
-# square root
+# --- ROUNDING ---
+# Rounding down
+print(math.floor(4.8))
+
+# Rounding up
+print(math.ceil(4.05))
+
+# --- POWER AND LOGARITHMIC ---
+# Square root
 print(math.sqrt(144))
 
-# to the power of - equivalent to 2**5
+# To the power of - equivalent to 2**5
 print(math.pow(2, 5))   # returns float
 print(2 ** 5)           # returns int
 
 # e**x
 print(math.exp(2))
 
-# logarithm of x to base (e is default base)
+# Logarithm of x to base (e is default base)
 print(math.log(101, 3))
 
 # base-2 logarithm of x
@@ -43,24 +50,24 @@ print(math.log2(512))
 # base-10 logarithm of x
 print(math.log10(1000))
 
-# ANGULAR CONVERSION
-# angle x from radians to degrees
+# --- ANGULAR CONVERSION ---
+# Angle x from radians to degrees
 print(math.degrees(math.pi))
 
-# angle x from degrees to radians
+# Angle x from degrees to radians
 print(math.radians(90))
 
-# TRIGONOMETRY
-# sine of x radians
+# --- TRIGONOMETRY ---
+# Sine of x radians
 print(math.sin(math.pi * 0.5))
 
-# cosine of x radians
+# Cosine of x radians
 print(math.cos(math.pi))
 
-# tan of x radians
+# Tan of x radians
 print(math.tan(math.pi * 0.25))
 
 # asin() - acos() - atan() for arcs of x in radians
 
-# hypotenuse - euclidean norm - sqrt(x*x + y*y)
+# Hypotenuse - euclidean norm - sqrt(x*x + y*y)
 print(math.hypot(3, 4))
