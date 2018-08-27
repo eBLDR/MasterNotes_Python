ee7edd88526301968f5d63fdf843f0d761ed2c82
eduard-passnfly <eduard@passnfly.com>
2018-08-27 10:09:20 +0200

    Socket server module for simpler server

---
+++ Network_Sockets/socketservermodule/client.py
@@ -0,0 +1,26 @@
+import socket
+import sys
+
+try:
+    HOST, PORT = 'localhost', int(sys.argv[1])
+except (IndexError, ValueError):
+    print('Usage: client.py <port> str str...')
+    sys.exit()
+
+data = ' '.join(sys.argv[2:])
+
+if not data:
+    print('Usage: client.py <port> str str...')
+    sys.exit()
+
+# Create a socket (SOCK_STREAM means a TCP socket)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    # Connect to server and send data
+    sock.connect((HOST, PORT))
+    sock.sendall(bytes(data + '\n', 'utf-8'))
+
+    # Receive data from the server and shut down
+    received = str(sock.recv(1024), 'utf-8')
+
+print('Sent:     {}'.format(data))
+print('Received: {}'.format(received))
---
+++ Network_Sockets/socketservermodule/server.py
@@ -0,0 +1,46 @@
+"""
+socketserver module uses sockets module to simplify the task of
+writing network servers.
+
+Types of servers:
+    - TCP Server
+    - UDP Server
+    - Unix Stream Server
+    - Unix Datagram Server
+"""
+
+import socketserver
+import sys
+
+
+class MyTCPHandler(socketserver.BaseRequestHandler):
+    """
+    The request handler class for our server.
+
+    It is instantiated once per connection to the server, and must
+    override the handle() method to implement communication to the
+    client.
+    """
+
+    def handle(self):
+        # self.request is the TCP socket connected to the client
+        self.data = self.request.recv(1024).strip()
+        print('{} wrote:'.format(self.client_address[0]))
+        print(self.data)
+        # just send back the same data, but upper-cased
+        self.request.sendall(self.data.upper())
+
+
+if __name__ == '__main__':
+    try:
+        HOST, PORT = 'localhost', int(sys.argv[1])
+    except (IndexError, ValueError):
+        print('Usage: server.py <port>')
+        sys.exit()
+
+    # Create the server, binding to localhost on port 9999
+    with socketserver.TCPServer((HOST, PORT), MyTCPHandler) as server:
+        print('Server is active.')
+        # Activate the server; this will keep running until you
+        # interrupt the program with Ctrl-C
+        server.serve_forever()
