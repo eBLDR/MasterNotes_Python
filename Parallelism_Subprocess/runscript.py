9e00ae8aed828f87ed0956c3ce65d88512cea6f5
eBLDR <ed.bldr@gmail.com>
2018-08-28 11:22:50 +0200

    Comments changed

--- Parallelism_Subprocess/runscript.py
+++ Parallelism_Subprocess/runscript.py
@@ -7,9 +7,9 @@ import subprocess
 # Launching a python script as an independent process - variables won't be shared
 # Popen can accept stdout, stdin, stderr as a communication pipe
 pyScript = subprocess.Popen(['python', 'launchedscript.py'], stdout=subprocess.PIPE)  # shell=True), it opens a new python shell
-pyScript.wait()
 
 # communicate() return the stdout and stderr pipes from the launched script as bytes type
+# It will halt the main process until the launched process has finished
 out, err = pyScript.communicate()
 
 print(type(out))
