4355776c4920b4bddd23fbdf6f044a7a22655fc3
eBLDR <ed.bldr@gmail.com>
2018-09-26 13:04:49 +0200

    template_folder attr

--- Network_WebApplication_Flask/app/__init__.py
+++ Network_WebApplication_Flask/app/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
 
-app = Flask(__name__)
+# Taking __name__ is correct if using a single module,
+# alternatives are a custom name or __name__.split('.')[0] if the file is in a folder
+app = Flask(__name__, template_folder='templates')
+
+# @template_folder refers to the folder that contains the templates that should be used
+# by the application. Defaults to 'templates' folder in the root path of the app
 
 # Import at at the bottom to avoid circular imports
 from app import routes
