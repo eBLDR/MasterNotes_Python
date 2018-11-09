3ffad07a53170da5c53ea3cac6d90487c0b00c87
eBLDR <ed.bldr@gmail.com>
2018-11-09 15:52:02 +0100

    Minor changes

--- ExceptionHandling/tracebackmodule.py
+++ ExceptionHandling/tracebackmodule.py
@@ -1,15 +1,16 @@
-# allow to save the traceback from exceptions raised as str
+# Allows to save the traceback from exceptions raised as string
 
 import traceback
 
 try:
-    raise Exception("This is an error msg.")
+    raise Exception('This is an error msg.')
 
 except Exception:
-    # the best way to keep track of errors raised is creating a file containing the info
+    # The best way to keep track of errors raised is creating a file containing the info
     with open('errorInfo.txt', 'w') as errorFile:
-        # traceback.format_exc() is a the str of traceback
+        # traceback.format_exc() returns the str of the traceback
         errorFile.write(traceback.format_exc())
 
     print(traceback.format_exc())
-    print("The traceback info was written to errorInfo.txt.")
+    print('Traceback info was written to errorInfo.txt.')
+
