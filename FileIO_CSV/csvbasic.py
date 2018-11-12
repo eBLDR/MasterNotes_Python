03b36327a92b7f7986a46664720b5738a4b7e315
eBLDR <ed.bldr@gmail.com>
2018-11-12 15:47:22 +0100

    DictReader and Writer methods

--- FileIO_CSV/csvbasic.py
+++ FileIO_CSV/csvbasic.py
@@ -8,26 +8,37 @@ We can think of it as an spreadsheet.
 
 import csv
 
-# reading csv files
+# Reading csv files
 with open('example.csv', 'r') as csvfile:
     # reader() method for reading file
-    # delimiter allows us to set any specific field delimiter - character between cells - default is ','
+    # @delimiter allows us to set any specific field delimiter - character between cells - default is ','
     readCSV = csv.reader(csvfile, delimiter=',')
     data = []
-    for row in readCSV:  # each row is delimited by newline character '\n'
-        # attribute line_num - tracks current reading line
-        print("Line #: {}".format(readCSV.line_num))
+    for row in readCSV:  # Each row is delimited by newline character '\n'
+        # Attribute line_num - tracks current reading line
+        print('Line #: {}'.format(readCSV.line_num))
         print(row)
         data.append(row)
 
-print("=" * 20)
+print('=' * 20)
 print(data)
-print("=" * 20)
+print('=' * 20)
 
-# writing csv files - newline='' is to avoid double spacing between rows, in some OS default newline is '\n'
+# Writing csv files - newline='' is to avoid double spacing between rows, in some OS default newline is '\n'
 with open('example2.csv', 'w', newline='') as csvfile:
     # writer() method for writing file
-    # lineterminator is the character between rows - default is '\n'
-    writeCSV = csv.writer(csvfile, delimiter=';', lineterminator='\n\n')  # a different delimiter
+    # @lineterminator is the character between rows - default is '\n'
+    writeCSV = csv.writer(csvfile, delimiter=';', lineterminator='\n\n')  # A different delimiter
     for row in data:
         writeCSV.writerow(row)
+
+# Dictionary to csv
+my_dict = {'alfa': 1, 'beta': 2}
+with open('example2.csv', 'a', newline='') as csvfile:
+    # DictWriter makes the conversion from dict to csv easy
+    w = csv.DictWriter(csvfile, my_dict.keys())
+    w.writeheader()
+    w.writerow(my_dict)
+
+# Use DictReader for reading csv to dict-like
+
