7b4c61f7d2ff89de9dc46b42a2ccc738620d3290
eBLDR <ed.bldr@gmail.com>
2019-01-09 13:47:09 +0100

    Clean up code

--- DataType_Dictionaries/dictionaries.py
+++ DataType_Dictionaries/dictionaries.py
@@ -1,23 +1,26 @@
 """
 A dictionary object consist of a key-value pairs - it's a "hash table".
 Keys are immutable (keys must be "hashable"), whereas values are mutable.
-The items inside a dictionary are ordered (from python 3.6, before were unordered).
+The items inside a dictionary are ordered (from python version +3.6, before were unordered).
+If the dictionary must be ordered while using older versions, use:
+from collections import OrderedDict
+ordered_dict = OrderedDict()
 """
 
-myDict = {'alpha': 1,
-          'beta': 2,
-          'gamma': 3,
-          'epsilon': 5}
+my_dict = {'alpha': 1,
+           'beta': 2,
+           'gamma': 3,
+           'epsilon': 5}
 
-print(myDict["beta"])
+print(my_dict['beta'])
 
-# to add entry
-myDict['delta'] = 'unknown'
-print('delta is {}'.format(myDict['delta']))
-myDict['delta'] = 4
-print('delta is now {}'.format(myDict['delta']))
+# Add an entry
+my_dict['delta'] = 'unknown'
+print('delta is {}'.format(my_dict['delta']))
+my_dict['delta'] = 4
+print('delta is now {}'.format(my_dict['delta']))
 
-# a dictionary can also be created using dict()
+# A dictionary can also be created using dict() method
 d = dict(one=1, two=2, three=3)
 print(d)
 
@@ -31,39 +34,40 @@ print(d == d2 == d3)  # True
 
 print('=' * 20)
 
-# to delete a key-value pair
-del myDict['gamma']
+# To delete a key-value pair
+del my_dict['gamma']
 
 # del myDict  # to delete the full dictionary
 
 # key-value pairs are created in different order every time, and the
 # order can be modified by changing the dictionary
-ordered_keys = list(myDict.keys())
-ordered_keys.sort()  # sort by alphanumerical
+ordered_keys = list(my_dict.keys())
+ordered_keys.sort()  # sorts alphanumerically
 
-# equivalent to ordered_keys = sorted(list(myDict.keys()))
+# Equivalent to ordered_keys = sorted(list(myDict.keys()))
 
 for key in ordered_keys:
-    print(key + ' - ' + str(myDict[key]))
+    print(key + ' - ' + str(my_dict[key]))
+
 # or even more concise
-for key in sorted(myDict.keys()):
-    print(key + ' - ' + str(myDict[key]))
+for key in sorted(my_dict.keys()):
+    print(key + ' - ' + str(my_dict[key]))
 
-myDict_tuple = tuple(myDict.items())  # creating a tuple from a dict
-print(myDict_tuple)
-print(type(myDict_tuple))
+my_dict_tuple = tuple(my_dict.items())  # Creating a tuple from a dict
+print(my_dict_tuple)
+print(type(my_dict_tuple))
 
-# useful for
-for k, v in myDict.items():
+# Useful for
+for k, v in my_dict.items():
     print(k, v)
 
-for i in myDict_tuple:
+for i in my_dict_tuple:
     name, value = i
     print(name + ' = ' + str(value))
 
-print(dict(myDict_tuple))  # creating a dict from a tuple
+print(dict(my_dict_tuple))  # Creating a dict from a tuple
 
-# dictionaries can be nested
+# Dictionaries can be nested
 nested_dict = {'nest_1': {'A': 1, 'B': 2},
                'nest_2': {'A': 10}}
 
@@ -71,22 +75,21 @@ print(nested_dict['nest_1'], nested_dict['nest_2']['A'])
 
 print('=' * 20)
 
-# merging dictionaries using the unpacked dictionary - **dict
-myDict2 = {'omega': 0}
-myDict3 = {**myDict, **myDict2}
-print(myDict3)
+# Merging dictionaries using the unpacked dictionary - **dict
+my_dict_2 = {'omega': 0}
+my_dict_3 = {**my_dict, **my_dict_2}
+print(my_dict_3)
 
 print('=' * 20)
 
-# filling a dictionary asking to the user
-userDict = {'a': 1}
+# Filling a dictionary asking to the user
+user_dict = {'a': 1}
 
-askingFor = input('key: ')
-searchingFor = userDict.get(askingFor, 'no')
+asking_for = input('key: ')
+searching_for = user_dict.get(asking_for, 'no')
 
-if searchingFor == 'no':
-    new = input('value for {0}: '.format(askingFor))
-    userDict[askingFor] = new
+if searching_for == 'no':
+    new = input('value for {0}: '.format(asking_for))
+    user_dict[asking_for] = new
 
-print(userDict)
-
+print(user_dict)
