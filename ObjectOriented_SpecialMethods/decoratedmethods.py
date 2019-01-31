caf3d5b22aace61bdc1916ed0166b629ffa0191a
eBLDR <ed.bldr@gmail.com>
2019-01-31 09:50:48 +0100

    Class method notes updated

--- ObjectOriented_SpecialMethods/decoratedmethods.py
+++ ObjectOriented_SpecialMethods/decoratedmethods.py
@@ -1,6 +1,10 @@
 class BaseClass:
     """ Simple account class with balance. """
 
+    def __init__(self, name):
+        self.name = name
+        self.counter = 0
+
     # @item are called DECORATORS
     # static methods don't have access to the attributes of an instance of a class nor to the attributes of the class
     @staticmethod
@@ -8,22 +12,21 @@ class BaseClass:
         # Doesn't take self argument
         return n + 1
 
+    # Class attribute
     status = 0
 
     # Class methods have access to the class attributes, not to the instance attributes.
     # The changes made by a class method will affect all the instances of the class and of the
     # instances of the subclasses, if any
     @classmethod
-    def activate_status(cls):
+    def upgrade_status(cls):
         # Takes cls instead of self
-        cls.status = 1
-
-    def __init__(self, name):
-        self.name = name
-        self.counter = 0
+        cls.status += 1
 
     def do_something(self):
-        self.counter = BaseClass.add_1(self.counter)
+        self.counter = self.add_1(self.counter)
+        # Equivalent to
+        # self.counter = BaseClass.add_1(self.counter)
 
 
 # See inheritance.py
@@ -41,8 +44,17 @@ print('parent.status:', parent.status)
 print('child_1.status:', child_1.status)
 print('child_2.status:', child_2.status)
 
-parent.activate_status()
-print('Calling class method parent.activate_status()...')
+# Calling a @classmethod through an instance will affect all other instances of the class and subclasses
+parent.upgrade_status()
+print('Calling class method parent.upgrade_status()...')
+
+print('parent.status:', parent.status)
+print('child_1.status:', child_1.status)
+print('child_2.status:', child_2.status)
+
+# Calling a @classmethod through the class itself will produce the same result
+BaseClass.upgrade_status()
+print('Calling class method BaseClass.upgrade_status()...')
 
 print('parent.status:', parent.status)
 print('child_1.status:', child_1.status)
@@ -57,4 +69,8 @@ print('Calling static method child_1.do_something()...')
 
 print('child_1.counter:', child_1.counter)
 
+# Calling a @staticmethod through the instance
 print('Calling static method child_1.add_1(3):', child_1.add_1(3))
+
+# Calling a @staticmethod through the class
+print('Calling static method BaseClass.add_1(4):', BaseClass.add_1(4))
