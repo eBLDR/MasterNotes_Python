c1f34b049869e01cddafc02c1fd79606a90ec781
eBLDR <ed.bldr@gmail.com>
2019-04-16 10:43:55 +0200

    Multiple inheritance and operator overloading

---
+++ ObjectOriented_Inheritance/multipleinheritance.py
@@ -0,0 +1,40 @@
+# Multiple inheritance is also possible, a sub class inheriting from many super classes
+import datetime
+
+
+class Time:
+    def __init__(self, hour, minute, second):
+        self.hour = hour
+        self.minute = minute
+        self.second = second
+
+
+class Date:
+    def __init__(self, year, month, day):
+        self.year = year
+        self.month = month
+        self.day = day
+
+
+# Multiple inheritance, will inherit attributes and methods from all the base classes
+class Clock(Time, Date):
+    def __init__(self, year, month, day, hour, minute, second):
+        # super() will call to the first base class declared
+        print('super().__init__ is: ', super().__init__)
+
+        # __init__ calls have to be called manually for each
+        Time.__init__(self, hour, minute, second)
+        Date.__init__(self, year, month, day)
+
+    def display(self):
+        print('{year}-{month}-{day} {hour}:{minute}:{second}'.format(
+            year=self.year, month=self.month, day=self.day,
+            hour=self.hour, minute=self.minute, second=self.second
+        ))
+
+
+now = datetime.datetime.utcnow()
+clock = Clock(year=now.year, month=now.month, day=now.day,
+              hour=now.hour, minute=now.minute, second=now.second)
+
+clock.display()
---
+++ ObjectOriented_SpecialMethods/operatoroverloading.py
@@ -0,0 +1,68 @@
+"""
+Operator overloading allows the same operator to
+behave differently according to the context.
+It can be overwritten.
+
+OPERATOR            EXPRESSION	    INTERNALLY
+
+Addition	        p1 + p2	        p1.__add__(p2)
+Subtraction	        p1 - p2	        p1.__sub__(p2)
+Multiplication	    p1 * p2	        p1.__mul__(p2)
+Power	            p1 ** p2	    p1.__pow__(p2)
+Division	        p1 / p2	        p1.__truediv__(p2)
+Floor Division	    p1 // p2	    p1.__floordiv__(p2)
+Remainder (modulo)	p1 % p2	        p1.__mod__(p2)
+Bitwise Left Shift	p1 << p2	    p1.__lshift__(p2)
+Bitwise Right Shift	p1 >> p2	    p1.__rshift__(p2)
+Bitwise AND	        p1 & p2	        p1.__and__(p2)
+Bitwise OR	        p1 | p2	        p1.__or__(p2)
+Bitwise XOR	        p1 ^ p2	        p1.__xor__(p2)
+Bitwise NOT	        ~p1	            p1.__invert__()
+Less than	        p1 < p2	        p1.__lt__(p2)
+Less than or
+    equal to        p1 <= p2	    p1.__le__(p2)
+Equal to            p1 == p2	    p1.__eq__(p2)
+Not equal to	    p1 != p2	    p1.__ne__(p2)
+Greater than	    p1 > p2	        p1.__gt__(p2)
+Greater than or
+    equal to	    p1 >= p2	    p1.__ge__(p2)
+
+"""
+
+
+class Point:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return '({x},{y})'.format(x=self.x, y=self.y)
+
+    # Overwriting the method being called with the "+" operator
+    def __add__(self, other):
+        # self refers to the object on the left of the +, and other to the one on the right
+        x = self.x + other.x
+        y = self.y + other.y
+
+        # Returns a new instance of itself
+        return Point(x, y)
+
+    # Overwriting the method being called with the "-" operator
+    def __sub__(self, other):
+        x = self.x - other.x
+        y = self.y - other.y
+        return Point(x, y)
+
+
+p1 = Point(2, 3)
+p2 = Point(-1, 5)
+
+print(p1 + p2)
+print(p1 - p2)
+
+"""
+Overloading methods are used in other languages (such as Java or C++) to use different versions
+of the method depending on the number and type of arguments that are passed to it.
+Python doesn't have such a thing, the arguments are assigned by order, by named parameters,
+or set to default if not specified. (See functions.)
+"""
