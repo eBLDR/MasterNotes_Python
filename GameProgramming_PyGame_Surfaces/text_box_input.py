d4ecd19372233e4186dff282870dc2f041f8f3d5
eBLDR <ed.bldr@gmail.com>
2019-08-03 20:18:20 +0200

    Added pygame text box input

---
+++ GameProgramming_PyGame_Surfaces/text_box_input.py
@@ -0,0 +1,86 @@
+import pygame as pygame
+
+
+class InputBox:
+    def __init__(self, x, y, w, h, text=''):
+        self.rect = pygame.Rect(x, y, w, h)
+        self.color = COLOR_INACTIVE
+        self.text = text
+        self.txt_surface = FONT.render(text, True, self.color)
+        self.active = False
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            # If the user clicked on the input_box rect.
+            mouse_x, mouse_y = event.pos
+            if self.rect.collidepoint(mouse_x, mouse_y):
+                # Toggle the active variable.
+                self.active = not self.active
+            else:
+                self.active = False
+            # Change the current color of the input box.
+            self.color = COLOR_ACTIVE if self.active else COLOR_INACTIVE
+
+        if event.type == pygame.KEYDOWN:
+            if self.active:
+
+                # Pressed enter - input is done
+                if event.key == pygame.K_RETURN:
+                    print(self.text)
+                    self.text = ''
+
+                # Delete
+                elif event.key == pygame.K_BACKSPACE:
+                    self.text = self.text[:-1]
+
+                # Add one character
+                else:
+                    self.text += event.unicode
+                # Re-render the text.
+                self.txt_surface = FONT.render(self.text, True, self.color)
+
+    def update(self):
+        # Resize the box if the text is too long.
+        width = max(200, self.txt_surface.get_width() + 10)
+        self.rect.w = width
+
+    def draw(self, screen):
+        # Blit the text.
+        screen.blit(self.txt_surface, (self.rect.x + 5, self.rect.y + 5))
+        # Blit the rect.
+        pygame.draw.rect(screen, self.color, self.rect, 2)
+
+
+def main():
+    clock = pygame.time.Clock()
+    input_box1 = InputBox(100, 100, 140, 32)
+    input_box2 = InputBox(100, 300, 140, 32)
+    input_boxes = [input_box1, input_box2]
+    done = False
+
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+            for box in input_boxes:
+                box.handle_event(event)
+
+        for box in input_boxes:
+            box.update()
+
+        screen.fill((30, 30, 30))
+        for box in input_boxes:
+            box.draw(screen)
+
+        pygame.display.flip()
+        clock.tick(30)
+
+
+pygame.init()
+screen = pygame.display.set_mode((640, 480))
+COLOR_INACTIVE = pygame.Color('lightskyblue3')
+COLOR_ACTIVE = pygame.Color('dodgerblue2')
+FONT = pygame.font.Font(None, 32)
+
+main()
+pygame.quit()
