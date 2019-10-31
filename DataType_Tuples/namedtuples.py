de81fe620823dc0d456d7780cfcd1ad0a94130c1
eBLDR <ed.bldr@gmail.com>
2019-10-31 16:21:32 +0100

    JSON to named tuple

--- DataType_Tuples/namedtuples.py
+++ DataType_Tuples/namedtuples.py
@@ -1,3 +1,5 @@
+import json
+
 # From collections module
 from collections import namedtuple
 
@@ -39,3 +41,18 @@ print(plants_list[0].lifecycle)
 for p in plants_list:
     if p.lifecycle == 'Green':
         print(p.name, p.scientific_name)
+
+print('#' * 30)
+
+# Dictionary to named tuple
+data = '{"name": "John Smith", "hometown": {"name": "New York", "id": 123}}'
+
+# Parse JSON into an object with attributes corresponding to dict keys.
+data_object = json.loads(data, object_hook=lambda d: namedtuple('DataObject', d.keys())(*d.values()))
+print(type(data_object))
+print(data_object.name, data_object.hometown.name, data_object.hometown.id)
+
+data_object_as_dict = data_object._asdict()
+
+print(type(data_object_as_dict))
+print(data_object_as_dict)
