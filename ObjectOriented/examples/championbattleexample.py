b514920e6bfe809af3be635387677570ff5c8911
eBLDR <ed.bldr@gmail.com>
2019-11-08 16:15:50 +0100

    Added attack speed

--- ObjectOriented/examples/championbattleexample.py
+++ ObjectOriented/examples/championbattleexample.py
@@ -1,16 +1,13 @@
 import random
 
 
-# TODO: add energy
-# TODO: in calculate_real_dmg function add a random chance to cast special skill
-# special skill uses energy
-
 class Stats:
     def __init__(self, base_stats):
-        self.hp = base_stats.hp
-        self.dmg = base_stats.dmg
-        self.crit_chance = base_stats.crit_chance
-        self.crit_factor = base_stats.crit_factor
+        self.hp = base_stats.hp  # > 0
+        self.dmg = base_stats.dmg  # > 0
+        self.atk_speed = base_stats.atk_speed  # > 0 ; attacks per second
+        self.crit_chance = base_stats.crit_chance  # (0, 1)
+        self.crit_factor = base_stats.crit_factor  # > 1
 
     def display(self):
         for stat, value in self.__dict__.items():
@@ -20,22 +17,25 @@ class Stats:
 
 class StatsSpartan:
     hp = 1300
-    dmg = 40
-    crit_chance = 0.2
-    crit_factor = 3
+    dmg = 45
+    atk_speed = 1
+    crit_chance = 0.25
+    crit_factor = 2.5
 
 
 class StatsSamurai:
     hp = 900
-    dmg = 55
+    dmg = 51
+    atk_speed = 1.3
     crit_chance = 0.5
-    crit_factor = 2
+    crit_factor = 1.8
 
 
 class StatsCrusader:
-    hp = 1500
-    dmg = 52
-    crit_chance = 0.05
+    hp = 1700
+    dmg = 60
+    atk_speed = 0.7
+    crit_chance = 0.2
     crit_factor = 1.5
 
 
@@ -46,6 +46,9 @@ class Champion:
 
         self.name = name
         self.stats = None
+
+        self.atk_is_ready = True
+        self.atk_cool_down = 0  # Seconds
 
         self._spawn()
 
@@ -59,11 +62,26 @@ class Champion:
     def respawn(self):
         self._spawn()
 
+    def die(self):
+        self.alive = False
+
     def display_stats(self):
         print('Champion: {}'.format(self.name))
         self.stats.display()
 
-    def attack(self, champion_victim):
+    def fight(self, time_elapsed, champion_victim):
+        if self.atk_is_ready:
+            self.hit(champion_victim)
+            self.atk_is_ready = False
+            self.atk_cool_down += 1 / self.stats.atk_speed
+
+        else:
+            self.atk_cool_down -= time_elapsed
+            if self.atk_cool_down <= 0:
+                self.atk_cool_down = 0
+                self.atk_is_ready = True
+
+    def hit(self, champion_victim):
         real_dmg = self.calculate_real_dmg()
         champion_victim.receive_dmg(
             real_dmg
@@ -84,24 +102,21 @@ class Champion:
         else:
             return self.stats.dmg
 
-    def die(self):
-        self.alive = False
 
-
-def battle(champion_1, champion_2, info=False):
-    if info:
+def battle(champion_1, champion_2):
+    if EXTENDED_INFO:
         champion_1.display_stats()
         print('-' * 20)
         champion_2.display_stats()
 
-    if info:
+    if EXTENDED_INFO:
         print('/\\' * 10 + '\n' + 'BATTLE!' * 3 + '\n' + '/\\' * 10)
 
     while champion_1.alive and champion_2.alive:
-        champion_1.attack(champion_2)
-        champion_2.attack(champion_1)
+        champion_1.fight(TIME_ELAPSED_UNIT, champion_2)
+        champion_2.fight(TIME_ELAPSED_UNIT, champion_1)
 
-    if info:
+    if EXTENDED_INFO:
         champion_1.display_stats()
         print('-' * 20)
         champion_2.display_stats()
@@ -115,7 +130,7 @@ def battle(champion_1, champion_2, info=False):
     else:
         winner = champion_1
 
-    if info:
+    if EXTENDED_INFO:
         print('=' * 20)
         if winner:
             print('Winner: {}'.format(winner.name))
@@ -126,22 +141,22 @@ def battle(champion_1, champion_2, info=False):
     return winner
 
 
-def tournament(champ_1, champ_2, number_of_battles=100, info=False):
+def tournament(champ_1, champ_2):
     results = {
         'draws': 0,
         champ_1.name: 0,
         champ_2.name: 0,
     }
 
-    for round_ in range(1, number_of_battles + 1):
-        if info:
+    for round_ in range(1, ROUNDS_PER_BATTLE + 1):
+        if EXTENDED_INFO:
             print('#' * 20)
             print('ROUND: {}'.format(round_))
 
         champ_1.respawn()
         champ_2.respawn()
 
-        winner = battle(champ_1, champ_2, info=info)
+        winner = battle(champ_1, champ_2)
 
         if winner:
             results[winner.name] += 1
@@ -152,7 +167,7 @@ def tournament(champ_1, champ_2, number_of_battles=100, info=False):
     return results
 
 
-def manager(champions, extended_info=False, number_of_battles=100):
+def manager(champions):
     # Fight them in all combinations
     for index, champ_1 in enumerate(champions):
         # Slice second iteration to avoid duplicate tournaments
@@ -164,8 +179,6 @@ def manager(champions, extended_info=False, number_of_battles=100):
             results = tournament(
                 champ_1,
                 champ_2,
-                number_of_battles=number_of_battles,
-                info=extended_info,
             )
 
             print('~' * 50)
@@ -182,7 +195,8 @@ if __name__ == '__main__':
     }
 
     EXTENDED_INFO = False
-    ROUNDS_PER_BATTLE = 3000
+    ROUNDS_PER_BATTLE = 100
+    TIME_ELAPSED_UNIT = 0.1  # In seconds
 
     # Init champion container
     CHAMPIONS = []
@@ -197,6 +211,4 @@ if __name__ == '__main__':
 
     manager(
         CHAMPIONS,
-        extended_info=EXTENDED_INFO,
-        number_of_battles=ROUNDS_PER_BATTLE,
     )
