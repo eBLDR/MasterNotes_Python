73fa863ecba03e7cd6b4d050e8638d0c7349a09b
eBLDR <ed.bldr@gmail.com>
2019-11-20 11:01:36 +0100

    Added exmaples on binary round problem

--- 00_CoreTheory/binaryroundproblem.py
+++ 00_CoreTheory/binaryroundproblem.py
@@ -5,10 +5,17 @@ import decimal
 """
 
 x = 10.10
-print(x)
+print('x = {}'.format(x))
+
 x += 0.10
-print(x)
+print('+0.10... x: {}'.format(x))
+
 x += 0.10
-print(x)
+print('+0.10... x: {}'.format(x))
+
 x -= 0.30
-print(x)
+print('-0.30... x: {}'.format(x))
+
+print('\n0.2 + 0.1 = {}'.format(0.2 + 0.1))
+
+print('\n25.45 - 22.5 = {}'.format(25.45 - 22.5))
