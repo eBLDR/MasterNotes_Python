9680b53c06c9564544e0122ca59aabb9cb2c35e4
eBLDR <ed.bldr@gmail.com>
2021-01-11 10:38:16 +0100

    Notes updated

--- Utils_DateTime/time_zones.py
+++ Utils_DateTime/time_zones.py
@@ -3,7 +3,7 @@ There are two types of datetime objects:
 - Naive, they do not contain information about timezones
 - Aware, they do contain information about timezones
 
-A timezone's offset refers to how many hours the timezone is from UTC.
+A time zone's offset refers to how many hours the timezone is from UTC.
 """
 import datetime
 import time
@@ -16,53 +16,54 @@ print(time.gmtime(0))  # beginning of the epoch
 time_zone_offset = time.timezone  # Offset of the local (non-DST) timezone
 time_zone_offset_dst = time.altzone  # Offset of the local (DST) timezone
 
-print('Non-DST offset: {}'.format(time_zone_offset))
-print('DST offset: {}'.format(time_zone_offset_dst))
-
-print('T\nhe current time zone: {}'.format(time.tzname[0]))
+print(f'Non-DST offset: {time_zone_offset}')
+print(f'DST offset: {time_zone_offset_dst}')
+print(f'Current time zone: {time.tzname[0]}')
 
 # DST (Daylight Saving Time)
+print(f'DST: {time.daylight}')
+
 if time.daylight != 0:
-    print('\tDST is in effect for this location')
-    print('\tDST timezone: ' + time.tzname[1])
+    print('\tDST is in effect for this location.')
+    print(f'\tDST timezone: {time.tzname[1]}')
 
 # UTC (Coordinated Universal Time) is a time standard
 # Aka Zulu Time (military and navigation)
-# strftime allow to set the displaying format
+# strftime() method allows to set the displaying format
 print('\nLocal time: ' + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
 print('UTC time: ' + time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime()))
 
 print('=' * 30)
-print('\nUSING DATETIME\n')
+print('USING DATETIME\n')
 
 # Naive time, has no timezone info
 local_time_naive = datetime.datetime.now()
 
-print('Naive local time: {}'.format(local_time_naive))
-print('Naive local time tzinfo: {}'.format(local_time_naive.tzinfo))
+print(f'Naive local time: {local_time_naive}')
+print(f'Naive local time tzinfo: {local_time_naive.tzinfo}')
 
 # Aware time, has tz info
 local_timezone = datetime.timezone(datetime.timedelta(seconds=time.altzone))
-print(local_timezone)
+print(f'Local timezone: {local_timezone}')
 
 local_time_aware = local_time_naive.astimezone(tz=local_timezone)
-print('Aware local time: {}'.format(local_time_aware))
-print('Aware local time tzinfo: {}'.format(local_time_aware.tzinfo))
+print(f'Aware local time: {local_time_aware}')
+print(f'Aware local time tzinfo: {local_time_aware.tzinfo}')
 
 utc_time_naive = datetime.datetime.utcnow()
-print('\nNaive UTC {}'.format(utc_time_naive))
+print(f'\nNaive UTC {utc_time_naive}')
 
 # Timezone for UTC
 utc_timezone = datetime.timezone.utc
 print(utc_timezone)
 
 utc_time_aware = utc_time_naive.astimezone(tz=utc_timezone)
-print('Aware UTC: {}'.format(utc_time_aware))
+print(f'Aware UTC: {utc_time_aware}')
 
 print('=' * 30)
 
-# Conversion from local to utc
+# Conversion from local to UTC
 my_time = datetime.datetime(2020, 12, 12, 5, 12, 24, tzinfo=local_timezone)
-print('My time in local: {}'.format(my_time))
+print(f'My time in local: {my_time}')
 my_time_utc = my_time.astimezone(tz=utc_timezone)
-print('My time in UTC: {}'.format(my_time_utc))
+print(f'My time in UTC: {my_time_utc}')
